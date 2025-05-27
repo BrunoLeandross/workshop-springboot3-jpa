0e42b685343881f616e0882c2dce489452a7c77f
Bruno Leandro <bruno.souza@groupsoftware.com.br>
2025-05-27 00:29:07 -0300

    User entity and resource

---
+++ src/main/java/com/projeto/course/entities/User.java
@@ -0,0 +1,68 @@
+package com.projeto.course.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable
+{
+    private Long ID;
+    private String Name;
+    private String Email;
+    private String Phone;
+    private String Password;
+
+    public User(){
+    }
+
+    public User(Long ID, String name, String email, String phone, String password) {
+        this.ID = ID;
+        Name = name;
+        Email = email;
+        Phone = phone;
+        Password = password;
+    }
+
+    public Long getID() {
+        return ID;
+    }
+    public void setID(Long ID) {
+        this.ID = ID;
+    }
+    public String getName() {
+        return Name;
+    }
+    public void setName(String name) {
+        Name = name;
+    }
+    public String getEmail() {
+        return Email;
+    }
+    public void setEmail(String email) {
+        Email = email;
+    }
+    public String getPhone() {
+        return Phone;
+    }
+    public void setPhone(String phone) {
+        Phone = phone;
+    }
+    public String getPassword() {
+        return Password;
+    }
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(ID, user.ID);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(ID);
+    }
+}
